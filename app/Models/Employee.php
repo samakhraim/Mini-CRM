2b9e8499403ff964ca27b7772c971a07992f9968
samakhraim <samakhraim12@gmail.com>
2024-01-19 19:06:50 +0200

    small edits

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
-    use SoftDeletes;
     use HasFactory;
     protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'company_id'];
     protected $dates = ['deleted_at'];
